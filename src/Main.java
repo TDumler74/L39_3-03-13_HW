a1778d7cfec7da879b2dbaf2fcca9a89efcbb3d6
TDumler74 <tatjana.dumler@gmail.com>
2023-03-14 01:33:38 +0100

    first1

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         Track track=new Track();
+        track.setProductionDate(2020);
         track.setColour("blue");
         track.setLoadCapacity(5000);
-        System.out.println("Track with colour "+track.getColour()+", have loadcapacity of "+track.getLoadCapacity());
-        //Track with colour blue, have loadcapacity of 5000
+        System.out.println("Track with productionsdate "+track.getProductionDate()+" and colour "+track.getColour()+", have loadcapacity of "+track.getLoadCapacity());
+        //Track with productionsdate 2020 and colour blue, have loadcapacity of 5000
         track.drive();
         //I can drive!
         System.out.println();
 
         PassengerCar passengerCar=new PassengerCar();
-        passengerCar.setModel(" Audi A3 ");
+        passengerCar.setModel("Audi A3 ");
         passengerCar.setColour(" white ");
         passengerCar.setNumberOfSeats(5);
-        System.out.println(passengerCar.getModel()+" with a colour"+passengerCar.getColour()+" have "+passengerCar.getNumberOfSeats()+" seats");
-        //Audi A3  with a colour white  have 5 seats
+        System.out.println(passengerCar.getModel()+" with a colour"+passengerCar.getColour()+" has "+passengerCar.getNumberOfSeats()+" seats");
+        //Audi A3  with a colour white  has 5 seats
+        passengerCar.drive();
+        //I can drive!
         passengerCar.stop();
         //I stop now!
     }
